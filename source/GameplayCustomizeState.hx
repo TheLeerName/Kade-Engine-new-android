46dc84e65e8e42dae51a94d058e93aba552b2e13
TheLeerName <lesha.nechaev.05@mail.ru>
2021-10-07 05:14:00 +0700

    ke 1.7-prerelease files
    https://github.com/KadeDev/Kade-Engine/tree/84a60b74414272427f48a5ec486340e83fe6f53d

---
+++ source/GameplayCustomizeState.hx
@@ -0,0 +1,375 @@
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.math.FlxMath;
+import flixel.FlxCamera;
+import flixel.math.FlxPoint;
+import flixel.FlxObject;
+#if FEATURE_DISCORD
+import Discord.DiscordClient;
+#end
+import flixel.group.FlxGroup.FlxTypedGroup;
+import openfl.ui.Keyboard;
+import flixel.FlxSprite;
+import flixel.FlxG;
+
+class GameplayCustomizeState extends MusicBeatState
+{
+	var defaultX:Float = FlxG.width * 0.55 - 135;
+	var defaultY:Float = FlxG.height / 2 - 50;
+
+	var background:FlxSprite;
+	var curt:FlxSprite;
+	var front:FlxSprite;
+
+	var sick:FlxSprite;
+
+	var text:FlxText;
+	var blackBorder:FlxSprite;
+
+	var boyfriend:Boyfriend;
+	var dad:Character;
+	var gf:Character;
+
+	var strumLine:FlxSprite;
+	var strumLineNotes:FlxTypedGroup<FlxSprite>;
+	var playerStrums:FlxTypedGroup<FlxSprite>;
+	private var camHUD:FlxCamera;
+	private var camGame:FlxCamera;
+
+	public override function create()
+	{
+		#if FEATURE_DISCORD
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("Customizing Gameplay Modules", null);
+		#end
+
+		background = new FlxSprite(-600, -200).loadGraphic(Paths.loadImage('stageback', 'shared'));
+		front = new FlxSprite(-650, 600).loadGraphic(Paths.loadImage('stagefront', 'shared'));
+		curt = new FlxSprite(-500, -300).loadGraphic(Paths.loadImage('stagecurtains', 'shared'));
+		background.antialiasing = FlxG.save.data.antialiasing;
+		front.antialiasing = FlxG.save.data.antialiasing;
+		curt.antialiasing = FlxG.save.data.antialiasing;
+
+		super.create();
+
+		camGame = new FlxCamera();
+		camHUD = new FlxCamera();
+		camHUD.bgColor.alpha = 0;
+
+		FlxG.cameras.reset(camGame);
+		FlxG.cameras.add(camHUD);
+
+		FlxCamera.defaultCameras = [camGame];
+
+		camHUD.zoom = FlxG.save.data.zoom;
+
+		persistentUpdate = persistentDraw = true;
+
+		background.scrollFactor.set(0.9, 0.9);
+		curt.scrollFactor.set(0.9, 0.9);
+		front.scrollFactor.set(0.9, 0.9);
+
+		add(background);
+		add(front);
+		add(curt);
+
+		var camFollow = new FlxObject(0, 0, 1, 1);
+
+		dad = new Character(100, 100, 'dad');
+
+		boyfriend = new Boyfriend(770, 450, 'bf');
+
+		gf = new Character(400, 130, 'gf');
+		gf.scrollFactor.set(0.95, 0.95);
+
+		var camPos:FlxPoint = new FlxPoint(dad.getGraphicMidpoint().x + 400, dad.getGraphicMidpoint().y);
+
+		camFollow.setPosition(camPos.x, camPos.y);
+
+		add(gf);
+		add(boyfriend);
+		add(dad);
+
+		add(camFollow);
+
+		FlxG.camera.follow(camFollow, LOCKON, 0.01);
+		FlxG.camera.zoom = 0.9;
+		FlxG.camera.focusOn(camFollow.getPosition());
+
+		strumLine = new FlxSprite(0, FlxG.save.data.strumline).makeGraphic(FlxG.width, 14);
+		strumLine.scrollFactor.set();
+		strumLine.alpha = 0.4;
+
+		add(strumLine);
+
+		if (FlxG.save.data.downscroll)
+			strumLine.y = FlxG.height - 165;
+
+		strumLineNotes = new FlxTypedGroup<FlxSprite>();
+		add(strumLineNotes);
+
+		playerStrums = new FlxTypedGroup<FlxSprite>();
+
+		sick = new FlxSprite().loadGraphic(Paths.loadImage('sick', 'shared'));
+		sick.setGraphicSize(Std.int(sick.width * 0.7));
+		sick.antialiasing = FlxG.save.data.antialiasing;
+		sick.scrollFactor.set();
+		sick.updateHitbox();
+		add(sick);
+
+		strumLine.cameras = [camHUD];
+		strumLineNotes.cameras = [camHUD];
+		sick.cameras = [camHUD];
+
+		generateStaticArrows(0);
+		generateStaticArrows(1);
+
+		text = new FlxText(5, FlxG.height + 40, 0,
+			"Click and drag around gameplay elements to customize their positions. Press R to reset. Q/E to change zoom. C to show combo. Escape to exit.",
+			12);
+		text.scrollFactor.set();
+		text.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+
+		blackBorder = new FlxSprite(-30, FlxG.height + 40).makeGraphic((Std.int(text.width + 900)), Std.int(text.height + 600), FlxColor.BLACK);
+		blackBorder.alpha = 0.5;
+
+		blackBorder.cameras = [camHUD];
+		text.cameras = [camHUD];
+
+		text.scrollFactor.set();
+		background.scrollFactor.set();
+
+		add(blackBorder);
+		add(text);
+
+		FlxTween.tween(text, {y: FlxG.height - 18}, 2, {ease: FlxEase.elasticInOut});
+		FlxTween.tween(blackBorder, {y: FlxG.height - 18}, 2, {ease: FlxEase.elasticInOut});
+
+		if (!FlxG.save.data.changedHit)
+		{
+			FlxG.save.data.changedHitX = defaultX;
+			FlxG.save.data.changedHitY = defaultY;
+		}
+
+		sick.x = FlxG.save.data.changedHitX;
+		sick.y = FlxG.save.data.changedHitY;
+
+		FlxG.mouse.visible = true;
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music != null)
+			Conductor.songPosition = FlxG.sound.music.time;
+
+		super.update(elapsed);
+
+		if (FlxG.save.data.zoom < 0.8)
+			FlxG.save.data.zoom = 0.8;
+
+		if (FlxG.save.data.zoom > 1.2)
+			FlxG.save.data.zoom = 1.2;
+
+		FlxG.camera.zoom = FlxMath.lerp(0.9, FlxG.camera.zoom, 0.95);
+		camHUD.zoom = FlxMath.lerp(FlxG.save.data.zoom, camHUD.zoom, 0.95);
+
+		if (FlxG.mouse.overlaps(sick) && FlxG.mouse.pressed)
+		{
+			sick.x = (FlxG.mouse.x - sick.width / 2) - 60;
+			sick.y = (FlxG.mouse.y - sick.height) - 60;
+		}
+
+		for (i in playerStrums)
+			i.y = strumLine.y;
+		for (i in strumLineNotes)
+			i.y = strumLine.y;
+
+		if (FlxG.keys.justPressed.Q)
+		{
+			FlxG.save.data.zoom += 0.02;
+			camHUD.zoom = FlxG.save.data.zoom;
+		}
+
+		if (FlxG.keys.justPressed.E)
+		{
+			FlxG.save.data.zoom -= 0.02;
+			camHUD.zoom = FlxG.save.data.zoom;
+		}
+
+		if (FlxG.mouse.overlaps(sick) && FlxG.mouse.justReleased)
+		{
+			FlxG.save.data.changedHitX = sick.x;
+			FlxG.save.data.changedHitY = sick.y;
+			FlxG.save.data.changedHit = true;
+		}
+
+		if (FlxG.keys.justPressed.C)
+		{
+			var visibleCombos:Array<FlxSprite> = [];
+
+			var seperatedScore:Array<Int> = [];
+
+			var comboSplit:Array<String> = (FlxG.random.int(0, 420) + "").split('');
+
+			// make sure we have 3 digits to display (looks weird otherwise lol)
+			if (comboSplit.length == 1)
+			{
+				seperatedScore.push(0);
+				seperatedScore.push(0);
+			}
+			else if (comboSplit.length == 2)
+				seperatedScore.push(0);
+
+			for (i in 0...comboSplit.length)
+			{
+				var str:String = comboSplit[i];
+				seperatedScore.push(Std.parseInt(str));
+			}
+
+			var daLoop:Int = 0;
+			for (i in seperatedScore)
+			{
+				var numScore:FlxSprite = new FlxSprite().loadGraphic(Paths.loadImage('num' + Std.int(i)));
+				numScore.screenCenter();
+				numScore.x = sick.x + (43 * daLoop) - 50;
+				numScore.y = sick.y + 100;
+				numScore.cameras = [camHUD];
+				numScore.antialiasing = FlxG.save.data.antialiasing;
+				numScore.setGraphicSize(Std.int(numScore.width * 0.5));
+				numScore.updateHitbox();
+
+				numScore.acceleration.y = FlxG.random.int(200, 300);
+				numScore.velocity.y -= FlxG.random.int(140, 160);
+				numScore.velocity.x = FlxG.random.float(-5, 5);
+
+				add(numScore);
+
+				visibleCombos.push(numScore);
+
+				FlxTween.tween(numScore, {alpha: 0}, 0.2, {
+					onComplete: function(tween:FlxTween)
+					{
+						visibleCombos.remove(numScore);
+						numScore.destroy();
+					},
+					onUpdate: function(tween:FlxTween)
+					{
+						if (!visibleCombos.contains(numScore))
+						{
+							tween.cancel();
+							numScore.destroy();
+						}
+					},
+					startDelay: Conductor.crochet * 0.002
+				});
+
+				if (visibleCombos.length > seperatedScore.length + 20)
+				{
+					for (i in 0...seperatedScore.length - 1)
+					{
+						visibleCombos.remove(visibleCombos[visibleCombos.length - 1]);
+					}
+				}
+
+				daLoop++;
+			}
+		}
+
+		if (FlxG.keys.justPressed.R)
+		{
+			sick.x = defaultX;
+			sick.y = defaultY;
+			FlxG.save.data.zoom = 1;
+			camHUD.zoom = FlxG.save.data.zoom;
+			FlxG.save.data.changedHitX = sick.x;
+			FlxG.save.data.changedHitY = sick.y;
+			FlxG.save.data.changedHit = false;
+		}
+
+		if (controls.BACK)
+		{
+			FlxG.mouse.visible = false;
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			FlxG.switchState(new OptionsMenu());
+		}
+	}
+
+	override function beatHit()
+	{
+		super.beatHit();
+
+		if (curBeat % 2 == 0)
+		{
+			boyfriend.dance();
+			dad.dance();
+		}
+		else if (dad.curCharacter == 'spooky' || dad.curCharacter == 'gf')
+			dad.dance();
+
+		gf.dance();
+
+		if (!FlxG.keys.pressed.SPACE)
+		{
+			FlxG.camera.zoom += 0.015;
+			camHUD.zoom += 0.010;
+		}
+
+		trace('beat');
+	}
+
+	// ripped from play state cuz im lazy
+
+	private function generateStaticArrows(player:Int):Void
+	{
+		for (i in 0...4)
+		{
+			// FlxG.log.add(i);
+			var babyArrow:FlxSprite = new FlxSprite(0, strumLine.y);
+			babyArrow.frames = Paths.getSparrowAtlas('NOTE_assets', 'shared');
+			babyArrow.animation.addByPrefix('green', 'arrowUP');
+			babyArrow.animation.addByPrefix('blue', 'arrowDOWN');
+			babyArrow.animation.addByPrefix('purple', 'arrowLEFT');
+			babyArrow.animation.addByPrefix('red', 'arrowRIGHT');
+			babyArrow.antialiasing = FlxG.save.data.antialiasing;
+			babyArrow.setGraphicSize(Std.int(babyArrow.width * 0.7));
+			switch (Math.abs(i))
+			{
+				case 0:
+					babyArrow.x += Note.swagWidth * 0;
+					babyArrow.animation.addByPrefix('static', 'arrowLEFT');
+					babyArrow.animation.addByPrefix('pressed', 'left press', 24, false);
+					babyArrow.animation.addByPrefix('confirm', 'left confirm', 24, false);
+				case 1:
+					babyArrow.x += Note.swagWidth * 1;
+					babyArrow.animation.addByPrefix('static', 'arrowDOWN');
+					babyArrow.animation.addByPrefix('pressed', 'down press', 24, false);
+					babyArrow.animation.addByPrefix('confirm', 'down confirm', 24, false);
+				case 2:
+					babyArrow.x += Note.swagWidth * 2;
+					babyArrow.animation.addByPrefix('static', 'arrowUP');
+					babyArrow.animation.addByPrefix('pressed', 'up press', 24, false);
+					babyArrow.animation.addByPrefix('confirm', 'up confirm', 24, false);
+				case 3:
+					babyArrow.x += Note.swagWidth * 3;
+					babyArrow.animation.addByPrefix('static', 'arrowRIGHT');
+					babyArrow.animation.addByPrefix('pressed', 'right press', 24, false);
+					babyArrow.animation.addByPrefix('confirm', 'right confirm', 24, false);
+			}
+			babyArrow.updateHitbox();
+			babyArrow.scrollFactor.set();
+
+			babyArrow.ID = i;
+
+			if (player == 1)
+				playerStrums.add(babyArrow);
+
+			babyArrow.animation.play('static');
+			babyArrow.x += 50;
+			babyArrow.x += ((FlxG.width / 2) * player);
+
+			strumLineNotes.add(babyArrow);
+		}
+	}
+}
